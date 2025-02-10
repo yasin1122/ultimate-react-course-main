a0cf401983645bc5ac14be38551f9be78ec384f0
Yasin Cakal <whoisyasin@gmail.com>
2025-02-10 14:34:33 -0800

    more components

--- 03-pizza-menu/starter/pizza-menu/src/main.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/main.jsx
@@ -49,12 +49,36 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello World!3</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   )
+}
+
+function Footer() {
+  return (
+    <footer>
+      {' '}
+      {new Date().toLocaleTimeString()}. We&apos;re currently open!
+    </footer>
+  )
+  // return React.createElement('footer', null, "We're currently open!")
 }
 
 function Pizza() {
