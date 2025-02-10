69ee53b457c64198d1846b324065840224ce05f8
Yasin Cakal <whoisyasin@gmail.com>
2025-02-10 10:11:51 -0800

    hello world working

--- 03-pizza-menu/starter/pizza-menu/src/main.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+function App() {
+  return <h1>Hello World!3</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
