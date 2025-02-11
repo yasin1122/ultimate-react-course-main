03f87a15b1b917ffa18a7e71d26c6c0371322d75
Yasin Cakal <whoisyasin@gmail.com>
2025-02-11 08:24:47 -0800

    day

--- 03-pizza-menu/starter/pizza-menu/src/main.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/main.jsx
@@ -71,10 +71,37 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='src/assets/pizzas/spinaci.jpg'
+        price={10}
+      />
+
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomato, mushrooms'
+        price={12}
+        photoName='src/assets/pizzas/funghi.jpg'
+      />
     </main>
+  )
+}
+
+function Pizza(props) {
+  console.log(props)
+  return (
+    <div className='pizza'>
+      <img
+        src={props.photoName}
+        alt={props.name}
+      />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   )
 }
 
@@ -94,19 +121,6 @@ function Footer() {
     </footer>
   )
   // return React.createElement('footer', null, "We're currently open!")
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img
-        src='src/assets/pizzas/spinaci.jpg'
-        alt='Pizza Spinaci'
-      />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
