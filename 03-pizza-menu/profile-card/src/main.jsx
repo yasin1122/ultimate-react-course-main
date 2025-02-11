e1e6f21a9061bb09616f5762815c52a9f73090ef
Yasin Cakal <whoisyasin@gmail.com>
2025-02-11 08:34:05 -0800

    profile card start

---
+++ 03-pizza-menu/profile-card/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return (
+    <>
+      <h1>Profile Card</h1>
+    </>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
