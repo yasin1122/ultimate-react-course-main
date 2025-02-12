89ce274465710e2791cdd3f6ddaeb49a5376d8e1
Yasin Cakal <whoisyasin@gmail.com>
2025-02-11 19:47:47 -0800

    smaller components

--- 03-pizza-menu/starter/pizza-menu/src/main.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/main.jsx
@@ -92,7 +92,8 @@ function Menu() {
 }
 
 function Pizza(props) {
-  console.log(props)
+  if (props.pizzaObj.soldOut) return null
+
   return (
     <li className='pizza'>
       <img
@@ -118,16 +119,24 @@ function Footer() {
   return (
     <footer className='footer'>
       {isOpen ? (
-        <div className='order'>
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className='btn'>Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           Please come back between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  )
+}
+
+function Order(props) {
+  return (
+    <div className='order'>
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className='btn'>Order</button>
+    </div>
   )
 }
 
