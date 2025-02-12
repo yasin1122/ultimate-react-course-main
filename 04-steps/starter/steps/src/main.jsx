b0076716761f8939ae9403f1ebd96458d5555d6e
Yasin Cakal <whoisyasin@gmail.com>
2025-02-11 22:12:36 -0800

    steps start

--- 04-steps/starter/steps/src/main.jsx
+++ 04-steps/starter/steps/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-function App() {
-  return <h1>Hello World</h1>
-}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
