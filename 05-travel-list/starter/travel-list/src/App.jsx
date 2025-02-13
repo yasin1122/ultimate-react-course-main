ba196abebb373b02f1c7b14122347801ea0f873e
Yasin Cakal <whoisyasin@gmail.com>
2025-02-13 11:51:05 -0800

    draft

--- 05-travel-list/starter/travel-list/src/App.jsx
+++ 05-travel-list/starter/travel-list/src/App.jsx
@@ -23,9 +23,18 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    if (!description) return
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem)
+
+    setDescription('')
+    setQuantity(1)
   }
 
   return (
@@ -33,7 +42,9 @@ function Form() {
       className='add-form'
       onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={e => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
           <option
             value={num}
@@ -45,8 +56,9 @@ function Form() {
       <input
         type='text'
         placeholder='Item...'
-        value={description}
-        onChange={e => setDescription(e.target.value)}
+        // This is how we manually control element
+        value={description} // By manually setting the value
+        onChange={e => setDescription(e.target.value)} // and update it
       />
       <button>Add</button>
     </form>
