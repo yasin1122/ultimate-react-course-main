111fc3d75b4b41a5e3dd9df0c95291a42f4e3a7c
Yasin Cakal <whoisyasin@gmail.com>
2025-02-12 21:27:22 -0800

    rough draft

--- 05-travel-list/starter/travel-list/src/App.jsx
+++ 05-travel-list/starter/travel-list/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
+export default function App() {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   )
 }
 
-export default App
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  )
+}
+
+function PackingList() {
+  return <div className='list'>LIST</div>
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
