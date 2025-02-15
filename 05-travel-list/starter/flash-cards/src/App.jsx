87b3761949d526b7d5092702ecc8872f8e60b84f
Yasin Cakal <whoisyasin@gmail.com>
2025-02-15 10:25:03 -0800

    flash cards done

--- 05-travel-list/starter/flash-cards/src/App.jsx
+++ 05-travel-list/starter/flash-cards/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export default function App() {
   return (
     <div className='App'>
@@ -41,11 +43,22 @@ const questions = [
 ]
 
 function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null)
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null)
+  }
+
   return (
     <div className='flashcards'>
       {questions.map(question => (
-        <div key={question.id}>
-          <p>{question.question}</p>
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? 'selected' : ''}>
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
         </div>
       ))}
     </div>
