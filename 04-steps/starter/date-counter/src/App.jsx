0b43aa1398744b165489028bd145ad795220b63c
Yasin Cakal <whoisyasin@gmail.com>
2025-02-15 10:44:27 -0800

    date counter v2

--- 04-steps/starter/date-counter/src/App.jsx
+++ 04-steps/starter/date-counter/src/App.jsx
@@ -2,22 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [step, setStep] = useState(0)
+  const [step, setStep] = useState(1)
   const [count, setCount] = useState(0)
 
   const date = new Date()
   date.setDate(date.getDate() + count)
 
   const handleReset = () => {
-    setStep(0)
+    setStep(1)
     setCount(0)
   }
 
   return (
     <>
       <div>
+        <input
+          type='range'
+          min='0'
+          max='10'
+          value={step}
+          onChange={e => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+      <div>
         <button onClick={() => setStep(step => step - 1)}>-</button>
-        <span> Step: {step} </span>
+        <input
+          type='text'
+          value={count}
+          onChange={e => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setStep(step => step + 1)}>+</button>
       </div>
       <div>
@@ -35,7 +49,9 @@ function App() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
-      <button onClick={() => handleReset()}>Reset</button>
+      {count !== 0 || step !== 1 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
     </>
   )
 }
