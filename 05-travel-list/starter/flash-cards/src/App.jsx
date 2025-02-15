71b13b8602f1acd6b8081a6f1b9987fc4543d6cf
Yasin Cakal <whoisyasin@gmail.com>
2025-02-14 20:22:34 -0800

    starter prep

--- 05-travel-list/starter/flash-cards/src/App.jsx
+++ 05-travel-list/starter/flash-cards/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <FlashCards />
+    </div>
   )
 }
 
-export default App
+const questions = [
+  {
+    id: 3457,
+    question: 'What language is React based on?',
+    answer: 'JavaScript'
+  },
+  {
+    id: 7336,
+    question: 'What are the building blocks of React apps?',
+    answer: 'Components'
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: 'JSX'
+  },
+  {
+    id: 1297,
+    question: 'How to pass data from parent to child components?',
+    answer: 'Props'
+  },
+  {
+    id: 9103,
+    question: 'How to give components memory?',
+    answer: 'useState hook'
+  },
+  {
+    id: 2002,
+    question:
+      'What do we call an input element that is completely synchronised with state?',
+    answer: 'Controlled element'
+  }
+]
+
+function FlashCards() {
+  return <div>TODO</div>
+}
