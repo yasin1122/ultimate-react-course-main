06426982855f21e18b3eb24836aea2cca6210421
Yasin Cakal <whoisyasin@gmail.com>
2025-02-16 10:17:17 -0800

    update

--- 05-travel-list/starter/travel-list/src/App.jsx
+++ 05-travel-list/starter/travel-list/src/App.jsx
@@ -7,11 +7,17 @@ const initialItems = [
 ]
 
 export default function App() {
+  const [items, setItems] = useState([])
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item])
+  }
+
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   )
@@ -21,14 +27,9 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('')
   const [quantity, setQuantity] = useState(1)
-  const [items, setItems] = useState([])
-
-  function handleAddItems(item) {
-    setItems(items => [...items, item])
-  }
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -37,7 +38,7 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() }
 
-    handleAddItems(newItem)
+    onAddItems(newItem)
 
     setDescription('')
     setQuantity(1)
@@ -72,11 +73,11 @@ function Form() {
   )
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className='list'>
       <ul>
-        {initialItems.map(item => (
+        {items.map(item => (
           <Item
             item={item}
             key={item.id}
