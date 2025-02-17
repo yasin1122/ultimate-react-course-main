c435bce14b7617a6e342d83a8e7a3b176131daab
Yasin Cakal <whoisyasin@gmail.com>
2025-02-16 20:13:43 -0800

    refactor

---
+++ 05-travel-list/starter/travel-list/src/Form.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+export function Form({ onAddItems }) {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!description) return
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+
+    onAddItems(newItem)
+
+    setDescription('')
+    setQuantity(1)
+  }
+
+  return (
+    <form
+      className='add-form'
+      onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        // e.target refers to the current html element
+        onChange={e => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option
+            value={num}
+            key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type='text'
+        placeholder='Item...'
+        // This is how we manually control element
+        value={description} // By manually setting the value
+        onChange={e => setDescription(e.target.value)} // and update it
+      />
+      <button>Add</button>
+    </form>
+  )
+}
---
+++ 05-travel-list/starter/travel-list/src/Item.jsx
@@ -0,0 +1,16 @@
+export function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    // Ensure that the value passed to style is an object (even if empty)
+    <li>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  )
+}
