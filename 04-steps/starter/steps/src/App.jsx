911827b7a49d143e23b27ad9135e0159fef317b8
Yasin Cakal <whoisyasin@gmail.com>
2025-02-16 21:54:27 -0800

    steps v2.2

--- 04-steps/starter/steps/src/App.jsx
+++ 04-steps/starter/steps/src/App.jsx
@@ -35,9 +35,7 @@ export default function App() {
             <div className={step >= 3 && 'active'}>3</div>
           </div>
 
-          <p className='message'>
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className='buttons'>
             <Button
@@ -57,6 +55,15 @@ export default function App() {
         </div>
       )}
     </>
+  )
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className='message'>
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   )
 }
 
