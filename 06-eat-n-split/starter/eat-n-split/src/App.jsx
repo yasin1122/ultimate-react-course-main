662ed9de92737ceecb666990c1e5119418d56949
Yasin Cakal <whoisyasin@gmail.com>
2025-02-17 20:02:23 -0800

    friends list static

--- 06-eat-n-split/starter/eat-n-split/src/App.jsx
+++ 06-eat-n-split/starter/eat-n-split/src/App.jsx
@@ -21,8 +21,50 @@ const initialFriends = [
 
 export default function App() {
   return (
-    <>
-      <h1>Eat-n-Split</h1>
-    </>
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList></FriendsList>
+      </div>
+    </div>
+  )
+}
+
+function FriendsList() {
+  const friends = initialFriends
+
+  return (
+    <ul>
+      {friends.map(friend => (
+        <Friend
+          friend={friend}
+          key={friend.id}></Friend>
+      ))}
+    </ul>
+  )
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img
+        src={friend.image}
+        alt={friend.name}
+      />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className='button'>Select</button>
+    </li>
   )
 }
