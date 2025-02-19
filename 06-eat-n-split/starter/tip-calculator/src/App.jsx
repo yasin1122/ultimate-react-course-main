6d427fbd2089175b3fe60e31cbac7636f9767536
Yasin Cakal <whoisyasin@gmail.com>
2025-02-18 19:00:08 -0800

    static

--- 06-eat-n-split/starter/tip-calculator/src/App.jsx
+++ 06-eat-n-split/starter/tip-calculator/src/App.jsx
@@ -1,5 +1,31 @@
 export default function App() {
-  return <h1>Tip Calculator</h1>
+  return (
+    <>
+      <h1>Tip Calculator</h1>
+      <form>
+        <label>How much was the bill?</label>
+        <input type='text'></input>
+        <br></br>
+        <label>How did you like the service?</label>
+        <select>
+          <option>Terrible! 0%</option>
+          <option>Just OK 5%</option>
+          <option>Good 10%</option>
+          <option>Amazing! 20%</option>
+        </select>
+        <br></br>
+        <label>How did your friend like the service?</label>
+        <select>
+          <option>Terrible! 0%</option>
+          <option>Just OK 5%</option>
+          <option>Good 10%</option>
+          <option>Amazing! 20%</option>
+        </select>
+      </form>
+      <h1>You pay $105 ($100 + $5 tip)</h1>
+      <button>Reset</button>
+    </>
+  )
 }
 
 // How much was the bill? [  ]
