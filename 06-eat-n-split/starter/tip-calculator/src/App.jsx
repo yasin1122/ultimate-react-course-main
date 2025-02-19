3f33e6b2d062fc3e7b92327697b48409013bcc68
Yasin Cakal <whoisyasin@gmail.com>
2025-02-18 18:19:54 -0800

    stories

--- 06-eat-n-split/starter/tip-calculator/src/App.jsx
+++ 06-eat-n-split/starter/tip-calculator/src/App.jsx
@@ -1,3 +1,14 @@
 export default function App() {
   return <h1>Tip Calculator</h1>
 }
+
+// How much was the bill? [  ]
+
+// How did you like the service?
+// Dissatisfied 0%, Just ok 5%, Pretty good 10%, Amazing! 20%
+
+// How did your friend like the service?
+
+// You pay $105 ($100 + $5 tip)
+
+// Reset
