d8f7002d558c70df11993019a191cf4ed8ee3b9f
Yasin Cakal <whoisyasin@gmail.com>
2025-02-18 21:10:27 -0800

    skeleton

--- practice/shopping-list/src/App.jsx
+++ practice/shopping-list/src/App.jsx
@@ -1,7 +1,36 @@
+const demoList = [
+  { name: 'bread', quantity: 2 },
+  { name: 'eggs', quantity: 12 },
+  { name: 'salmon', quantity: 1 }
+]
+
 export default function App() {
   return (
     <>
       <h1>Shopping List</h1>
+      <ShoppingList />
     </>
+  )
+}
+
+function ShoppingList() {
+  return (
+    <ul>
+      {demoList.map(item => (
+        <ShoppingItem
+          name={item.name}
+          quantity={item.quantity}
+          key={item.name}
+        />
+      ))}
+    </ul>
+  )
+}
+
+function ShoppingItem({ name, quantity }) {
+  return (
+    <li>
+      {name} {quantity}
+    </li>
   )
 }
