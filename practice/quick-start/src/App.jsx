bebe6668906ae6bb9c63951858f27bd6cff0ddd1
Yasin Cakal <whoisyasin@gmail.com>
2025-02-19 19:37:05 -0800

    quick start done

--- practice/quick-start/src/App.jsx
+++ practice/quick-start/src/App.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react'
+
 export default function App() {
+  const [linkedCount, setLinkedCount] = useState(0)
+
+  function handleClick() {
+    setLinkedCount(linkedCount + 1)
+  }
+
   return (
     <div>
       <h1>Quick Start</h1>
-      <MyButton />
+      <MyButton
+        linkedCount={linkedCount}
+        onHandleClick={handleClick}
+      />
+      <MyButton
+        linkedCount={linkedCount}
+        onHandleClick={handleClick}
+      />
       <Profile />
+      <ShoppingList />
     </div>
   )
 }
 
-function MyButton() {
-  return <button>This is a button</button>
+function MyButton({ linkedCount, onHandleClick }) {
+  return <button onClick={onHandleClick}>Clicked Count = {linkedCount}</button>
 }
 
 const user = {
@@ -34,4 +50,21 @@ function Profile() {
       />
     </>
   )
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 }
+]
+
+function ShoppingList() {
+  const listItems = products.map(product => (
+    <li
+      key={product.id}
+      style={{ color: product.isFruit ? 'magenta' : 'darkgreen' }}>
+      {product.title}
+    </li>
+  ))
+  return <ul>{listItems}</ul>
 }
