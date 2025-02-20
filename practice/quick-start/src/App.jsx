6bd392f897461aa0b378c15c2168e9cb04fb0479
Yasin Cakal <whoisyasin@gmail.com>
2025-02-19 19:16:29 -0800

    1

--- practice/quick-start/src/App.jsx
+++ practice/quick-start/src/App.jsx
@@ -1,3 +1,37 @@
 export default function App() {
-  return <h1>Quick Start</h1>
+  return (
+    <div>
+      <h1>Quick Start</h1>
+      <MyButton />
+      <Profile />
+    </div>
+  )
+}
+
+function MyButton() {
+  return <button>This is a button</button>
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90
+}
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className='avatar'
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+          borderRadius: '50%'
+        }}
+      />
+    </>
+  )
 }
