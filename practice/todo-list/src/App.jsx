a5998688e89a6a7eadb7ad3bd9391430bc834a62
Yasin Cakal <whoisyasin@gmail.com>
2025-02-20 19:37:02 -0800

    static

--- practice/todo-list/src/App.jsx
+++ practice/todo-list/src/App.jsx
@@ -1,7 +1,33 @@
 export default function App() {
   return (
     <>
-      <h1>Todo List</h1>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input
+            type='text'
+            id='item'
+          />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>Todo List</h1>
+      <ul className='list'>
+        <li>
+          <label htmlFor=''>
+            <input type='checkbox' />
+            Item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+        <li>
+          <label htmlFor=''>
+            <input type='checkbox' />
+            Item 2
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+      </ul>
     </>
   )
 }
