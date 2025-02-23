fffe3650577197c7ae0122e0fc6ffe8e4be317e6
Yasin Cakal <whoisyasin@gmail.com>
2025-02-22 18:53:47 -0800

    default prop value

--- 07-usepopcorn/starter/use-popcorn/src/main.jsx
+++ 07-usepopcorn/starter/use-popcorn/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import StarRating from './StarRating'
+// import './index.css'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </StrictMode>
 )
