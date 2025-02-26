f9d11071a8f852aeb15394cda13cf75481fd2daa
Yasin Cakal <whoisyasin@gmail.com>
2025-02-25 21:50:41 -0800

    draft v2

--- 07-usepopcorn/starter/text-expander/src/App.jsx
+++ 07-usepopcorn/starter/text-expander/src/App.jsx
@@ -37,22 +37,35 @@ export default function App() {
 }
 
 function TextExpander({
-  collapsedNumWords = 20,
+  collapsedNumWords = 10,
   expandButtonText = 'Show more',
   collapseButtonText = 'Show less',
-  buttonColor,
+  buttonColor = '#1f09cd',
   expanded = false,
   className,
   children
 }) {
   const [isExpanded, setIsExpanded] = useState(expanded)
 
-  const displayText = isExpanded ? children : 'test'
+  const displayText = isExpanded
+    ? children
+    : children.split(' ').slice(0, collapsedNumWords).join(' ') + '...'
+
+  const buttonStyle = {
+    background: 'none',
+    border: 'none',
+    font: 'inherit',
+    cursor: 'pointer',
+    marginLeft: '6px',
+    color: buttonColor
+  }
 
   return (
     <div className={className}>
       <span>{displayText}</span>
-      <button onClick={() => setIsExpanded(exp => !exp)}>
+      <button
+        onClick={() => setIsExpanded(exp => !exp)}
+        style={buttonStyle}>
         {isExpanded ? collapseButtonText : expandButtonText}
       </button>
     </div>
