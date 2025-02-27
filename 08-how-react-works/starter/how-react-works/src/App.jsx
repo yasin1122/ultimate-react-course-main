018bfaea4e4283d22366f04604772bdb01ca16ed
Yasin Cakal <whoisyasin@gmail.com>
2025-02-27 08:23:57 -0800

    day

--- 08-how-react-works/starter/how-react-works/src/App.jsx
+++ 08-how-react-works/starter/how-react-works/src/App.jsx
@@ -84,6 +84,17 @@ function TabContent({ item }) {
     setLikes(likes + 1)
   }
 
+  function handleTripleInc() {
+    setLikes(likes + 1)
+    setLikes(likes + 1)
+    setLikes(likes + 1)
+  }
+
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+  }
+
   return (
     <div className='tab-content'>
       <h4>{item.summary}</h4>
@@ -97,12 +108,12 @@ function TabContent({ item }) {
         <div className='hearts-counter'>
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className='tab-undo'>
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
