41eeaf9449d8c51db7d1e10011bd2d06f1c57a27
Yasin Cakal <whoisyasin@gmail.com>
2025-02-28 15:23:10 -0800

    draft

--- 07-usepopcorn/starter/use-popcorn-v2/src/App.jsx
+++ 07-usepopcorn/starter/use-popcorn-v2/src/App.jsx
@@ -67,28 +67,39 @@ export default function App() {
   //     .then(data => setMovies(data.Search))
   // }, [])
 
-  useEffect(function () {
-    async function fetchMovies() {
-      try {
-        setIsLoading(true)
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${tempQuery}`
-        )
+  useEffect(
+    function () {
+      async function fetchMovies() {
+        try {
+          setIsLoading(true)
+          setError('')
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          )
 
-        if (!res.ok)
-          throw new Error('Something went wrong with fetching movies')
+          if (!res.ok)
+            throw new Error('Something went wrong with fetching movies')
 
-        const data = await res.json()
-        if (data.Response === 'False') throw new Error('Movie not found')
-        setMovies(data.Search)
-      } catch (err) {
-        setError(err.message)
-      } finally {
-        setIsLoading(false)
+          const data = await res.json()
+          if (data.Response === 'False') throw new Error('Movie not found')
+          setMovies(data.Search)
+        } catch (err) {
+          setError(err.message)
+        } finally {
+          setIsLoading(false)
+        }
       }
-    }
-    fetchMovies()
-  }, [])
+
+      if (query.length < 3) {
+        setMovies([])
+        setError('')
+        return
+      }
+
+      fetchMovies()
+    },
+    [query]
+  )
 
   return (
     <>
