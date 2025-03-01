6431e3e9dc59c632800a0bcfc50f9d600ad811e4
Yasin Cakal <whoisyasin@gmail.com>
2025-03-01 08:19:17 -0800

    starter

--- 09-classy-weather/starter/currency-converter/src/App.jsx
+++ 09-classy-weather/starter/currency-converter/src/App.jsx
@@ -1,3 +1,22 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
 export default function App() {
-  return <h1>Currency Converter</h1>
+  return (
+    <div>
+      <input type='text' />
+      <select>
+        <option value='USD'>USD</option>
+        <option value='EUR'>EUR</option>
+        <option value='CAD'>CAD</option>
+        <option value='INR'>INR</option>
+      </select>
+      <select>
+        <option value='USD'>USD</option>
+        <option value='EUR'>EUR</option>
+        <option value='CAD'>CAD</option>
+        <option value='INR'>INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  )
 }
