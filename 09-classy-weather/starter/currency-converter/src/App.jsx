c9e42279491ace5c2199cfddf788ffaa2a0d435e
Yasin Cakal <whoisyasin@gmail.com>
2025-03-20 09:06:00 -0700

    day

--- 09-classy-weather/starter/currency-converter/src/App.jsx
+++ 09-classy-weather/starter/currency-converter/src/App.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 
 export default function App() {
-  const [amount, setAmount] = useState(12)
+  const [amount, setAmount] = useState(16)
   const [fromCur, setFromCur] = useState('USD')
   const [toCur, setToCur] = useState('TRY')
   const [converted, setConverted] = useState('')
